9e6378171e4922a77fab4dce73d90538f83b15d4
unknown <kyrre.l.moe@gmail.com>
2013-10-21 18:17:29 +0200

    evaluate

--- src/oving4/Board.java
+++ src/oving4/Board.java
@@ -3,30 +3,67 @@ package oving4;
 public class Board {
 
 	
-	private final int width;
-	private final int height;
+	private final int lengthOfSides;
 	private final int maxNumberOfEggs;
-	private boolean[][] board;
+	private Node[][] board;
+	private final int maxScore;
+	private final int penaltyModifier;
 
-	public Board(int width, int height, int maxNumberOfEggs){
-		this.width = width;
-		this.height = height;
+	public Board(int lengthOfSides, int maxNumberOfEggs, int penaltyModifier){
+		this.lengthOfSides = lengthOfSides;
 		this.maxNumberOfEggs = maxNumberOfEggs;
-		board = new  boolean[height][width]; 
+		populateBoard();
+		this.maxScore = lengthOfSides*maxNumberOfEggs;
+		this.penaltyModifier = penaltyModifier;
+		
+		
 	}
-	public void insertEgg(int width, int height){
-		board[height][width] = true;
+	private void populateBoard() {
+		for (int i = 0; i < lengthOfSides; i++) {
+			for (int j = 0; j < lengthOfSides; j++) {
+				board[i][j] = new Node(i, j);
+			}
+			
+		}
 	}
-	public void removeEgg(int width, int height){
-		board[height][width] = false;
+	public void setNode(int width, int height, Node n){
+		board[height][width] = n;
+	}
+	public Node getNode(int width, int height){
+		return board[height][width];
 	}
 	public boolean hasEgg(int width, int height){
-		return board[height][width];
+		return board[height][width].isEgg();
+	}
+	
+	public double evaluate() {
+		return (getNumberOfEggs()-calculateNumberOfFaults()*penaltyModifier)/maxNumberOfEggs;
+	}
+	private int getNumberOfEggs() {
+		int n = 0;
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				if (hasEgg(i, j)) {
+					n++;
+				}
+			}
+		}
+		return n;
+	}
+	private int calculateNumberOfFaults(){
+		int numberOfFaults = 0;
+		for (int i = 0; i < board.length; i++) {
+			if (!isColumnLegal(i))
+				numberOfFaults++;
+			if (!isRowLegal(i))
+				numberOfFaults++;
+		}
+		return numberOfFaults;
 	}
 	public boolean isColumnLegal(int column){
 		int count = 0;
 		for (int i = 0; i < board.length; i++) {
-			if (board[i][column]) {
+			if (board[i][column].isEgg()) {
 				count++;
 			}
 		}
@@ -38,7 +75,7 @@ public class Board {
 	public boolean isRowLegal(int row){
 		int count = 0;
 		for (int i = 0; i < board[row].length; i++) {
-			if (board[row][i]) {
+			if (board[row][i].isEgg()) {
 				count++;
 			}
 		}
@@ -47,11 +84,7 @@ public class Board {
 		}
 		return true;
 	}
-	
-	public double evaluate() {
-		return Math.random();
-	}
-	public boolean isDiagonalLegalBottomToTop(int height, int width){
+	public boolean isDiagonalLegal(int height, int width){
 		return true;
 //		int count = 0;
 //		if(height == 0){
